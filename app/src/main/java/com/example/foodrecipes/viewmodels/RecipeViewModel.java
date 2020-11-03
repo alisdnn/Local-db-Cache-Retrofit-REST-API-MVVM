21d1498572ed7d8dda6546bd3526f59f73bee2d9
ali sadeghian <aliosdn@gmail.com>
2020-11-04 01:12:20 +0330

    First commit

---
+++ app/src/main/java/com/example/foodrecipes/viewmodels/RecipeViewModel.java
@@ -0,0 +1,47 @@
+package com.example.foodrecipes.viewmodels;
+
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.support.annotation.NonNull;
+
+import com.example.foodrecipes.models.Recipe;
+import com.example.foodrecipes.repositories.RecipeRepository;
+import com.example.foodrecipes.util.Resource;
+
+
+public class RecipeViewModel extends AndroidViewModel {
+
+    private RecipeRepository recipeRepository;
+
+    public RecipeViewModel(@NonNull Application application) {
+        super(application);
+        recipeRepository = RecipeRepository.getInstance(application);
+    }
+
+    public LiveData<Resource<Recipe>> searchRecipeApi(String recipeId){
+        return recipeRepository.searchRecipesApi(recipeId);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
